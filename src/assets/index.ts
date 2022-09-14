9a4f72246d9da0c1dd7301fe83bd869ae8128841
seongjin <seongjin2427@gmail.com>
2022-09-14 20:59:57 +0900

    feat: settings icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,4 @@ export { default as ChevronLeftIcon } from './icons/chevron-left.svg';
 export { default as SearchIcon } from './icons/search.svg';
 export { default as UserCircleIcon } from './icons/user-circle.svg';
 export { default as XMark } from './icons/x-mark.svg';
+export { default as SettingsIcon } from './icons/settings.svg';
