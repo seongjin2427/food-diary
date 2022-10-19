4a460ff5b552d0d23d48ab4b4ba489f71740e4b0
seongjin <seongjin2427@gmail.com>
2022-10-19 14:57:16 +0900

    chore: dialectModule 추가

--- src/db/config/config.ts
+++ src/db/config/config.ts
@@ -1,5 +1,6 @@
 /* eslint-disable  @typescript-eslint/no-non-null-assertion */
 import fs from 'fs';
+import mysql2 from 'mysql2'
 
 type Dialect = 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake';
 
@@ -11,6 +12,7 @@ export default {
     host: process.env.DB_HOSTNAME!,
     port: +process.env.DB_PORT!,
     dialect: 'mysql' as Dialect,
+    dialectModule: mysql2,
     dialectOptions: {
       bigNumberStrings: true,
     },
@@ -23,6 +25,7 @@ export default {
     host: process.env.DB_HOSTNAME!,
     port: +process.env.DB_PORT!,
     dialect: 'mysql' as Dialect,
+    dialectModule: mysql2,
     dialectOptions: {
       bigNumberStrings: true,
     },
@@ -35,6 +38,7 @@ export default {
     host: process.env.DB_HOSTNAME!,
     port: +process.env.DB_PORT!,
     dialect: 'mysql' as Dialect,
+    dialectModule: mysql2,
     dialectOptions: {
       bigNumberStrings: true,
       // ssl: {
