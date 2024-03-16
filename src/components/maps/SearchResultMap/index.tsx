5abe0349be73e767acef1398f9ca911b10bbc18f
seongjin <seongjin2427@gmail.com>
2024-03-16 13:25:30 +0900

    refact: 필요 없는 프래그먼트 삭제

--- src/components/maps/SearchResultMap/index.tsx
+++ src/components/maps/SearchResultMap/index.tsx
@@ -92,33 +92,29 @@ const SearchResultMap = ({
           </S.MoveButton>
         </S.ResultsContainer>
       )}
-      {showList && (
-        <>
-          {searchPlaceResults &&
-            searchPlaceResults.map((place, idx) => (
-              <S.PlaceListContainer key={idx} onClick={() => moveToPlacePageByPid(place)}>
-                <S.PlaceTitleBox>
-                  <S.PlaceName>{place.place_name}</S.PlaceName>
-                </S.PlaceTitleBox>
-                <S.PlaceContentBox>
-                  <S.PlaceAddress>{place.address_name}</S.PlaceAddress>
-                  <S.PlacePhone>{place.phone}</S.PlacePhone>
-                  <S.PlaceDistance>{+place.distance / 1000}km</S.PlaceDistance>
-                </S.PlaceContentBox>
-                <S.PlaceKind>
-                  {folderResults?.map(
-                    ({ fid, icon, color, places }) =>
-                      places.find((p) => p.id === place.id) && (
-                        <S.FolderIcon key={fid} selectedColor={color}>
-                          <SVGIcon icon={icon} width='1.25rem' height='1.25rem' />
-                        </S.FolderIcon>
-                      ),
-                  )}
-                </S.PlaceKind>
-              </S.PlaceListContainer>
-            ))}
-        </>
-      )}
+      {showList &&
+        searchPlaceResults?.map((place, idx) => (
+          <S.PlaceListContainer key={idx} onClick={() => moveToPlacePageByPid(place)}>
+            <S.PlaceTitleBox>
+              <S.PlaceName>{place.place_name}</S.PlaceName>
+            </S.PlaceTitleBox>
+            <S.PlaceContentBox>
+              <S.PlaceAddress>{place.address_name}</S.PlaceAddress>
+              <S.PlacePhone>{place.phone}</S.PlacePhone>
+              <S.PlaceDistance>{+place.distance / 1000}km</S.PlaceDistance>
+            </S.PlaceContentBox>
+            <S.PlaceKind>
+              {folderResults?.map(
+                ({ fid, icon, color, places }) =>
+                  places.find((p) => p.id === place.id) && (
+                    <S.FolderIcon key={fid} selectedColor={color}>
+                      <SVGIcon icon={icon} width='1.25rem' height='1.25rem' />
+                    </S.FolderIcon>
+                  ),
+              )}
+            </S.PlaceKind>
+          </S.PlaceListContainer>
+        ))}
       {searchPlaceResults && searchPlaceResults.length <= 0 && (
         <S.NoPlaces>검색된 장소가 없습니다</S.NoPlaces>
       )}
